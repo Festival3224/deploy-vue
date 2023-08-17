2623a5b548341d384a9e4dad33961c908966a59b
Festival3224 <QSR^V9&h>
2023-08-17 13:53:27 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>About EK Web Art</h1>
+    <p class="centered">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, doloremque ut libero repellendus sint impedit! Accusamus reiciendis assumenda iusto possimus tempore magni consequuntur quis, nesciunt fugiat totam, aspernatur tempora dolorum. Quibusdam qui sed quia pariatur excepturi, et enim ipsum mollitia. Placeat eius nostrum voluptatem enim ut doloribus molestiae accusamus ab?</p>
+    <p class="centered"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, deleniti.</p>
+  </div>
+</template>
+
+<style>
+
+.centered {
+  display: block;
+  width: 100%;
+  max-width: 450px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.svg" alt="vue logo">
+    <h1>EK Web Art</h1>
+    <h3>welcome to my page</h3>
+  </div>
+</template>
